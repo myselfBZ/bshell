2232a243f4dbedc5c804bacbdcc738baa5ea0341
myselfBZ <boburforfun@gmail.com>
2026-06-03 13:25:31 +0500

    pwd and cd

--- shell/shell.go
+++ shell/shell.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"os/user"
 	"sync"
 
 	"github.com/myselfBZ/bshell/ast"
@@ -15,22 +16,31 @@ func New() *Shell {
 	if err != nil {
 		panic(err)
 	}
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
 	return &Shell{
-		cwd: cwd,
+		cwd:  cwd,
+		user: user,
+		builtIns: map[string]bool{
+			"cd":  true,
+			"pwd": true,
+		},
 	}
 }
 
 type Shell struct {
-	cwd string
-	builtInCmds map[string]func(args ...string) error
+	cwd      string
+	user     *user.User
+	builtIns map[string]bool
 }
-
 
 func (s *Shell) Eval(cmds ...ast.Command) error {
 	for _, c := range cmds {
 		switch node := c.(type) {
 		case *ast.SimpleCommand:
-			return s.execSimpleCommand(node, os.Stdout, os.Stdin, os.Stderr) 
+			return s.execSimpleCommand(node, os.Stdout, os.Stdin, os.Stderr)
 		case *ast.InfixExpressionCmd:
 
 			switch node.Operator {
@@ -54,7 +64,6 @@ func (s *Shell) Eval(cmds ...ast.Command) error {
 					pr.Close()
 				})
 
-
 				left, ok := node.Left.(*ast.SimpleCommand)
 
 				if ok {
@@ -75,7 +84,7 @@ func (s *Shell) Eval(cmds ...ast.Command) error {
 					return s.execSimpleCommand(right, os.Stdout, os.Stderr, os.Stdin)
 				}
 
-				if err != nil  && node.Operator == "||"{
+				if err != nil && node.Operator == "||" {
 					right := node.Right.(*ast.SimpleCommand)
 					return s.execSimpleCommand(right, os.Stdout, os.Stderr, os.Stdin)
 				}
@@ -86,7 +95,6 @@ func (s *Shell) Eval(cmds ...ast.Command) error {
 	}
 	return nil
 }
-
 
 func (s *Shell) executePipe(node *ast.InfixExpressionCmd, out io.Writer) error {
 	pr, pw, err := os.Pipe()
@@ -122,12 +130,9 @@ func (s *Shell) executePipe(node *ast.InfixExpressionCmd, out io.Writer) error {
 	return err
 }
 
-
-
-
 func (s *Shell) execSimpleCommand(cmd *ast.SimpleCommand, out io.Writer, stderr io.Writer, in io.Reader) error {
 	for _, r := range cmd.Redirects {
-		switch r.Type{
+		switch r.Type {
 		case ast.RedirectStdout:
 			f, err := os.OpenFile(r.Target, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
 			if err != nil {
@@ -167,9 +172,41 @@ func (s *Shell) execSimpleCommand(cmd *ast.SimpleCommand, out io.Writer, stderr 
 		}
 	}
 
+	if s.isBuiltIn(cmd.Name) {
+		err := s.handleBuiltIn(&BuiltInCmd{
+			Name: cmd.Name,
+			Args: cmd.Args,
+			In:   in,
+			Out:  out,
+			Err:  stderr,
+		})
+
+		if err != nil {
+			fmt.Fprintln(stderr, err)
+		}
+
+		return err
+	}
+
 	c := exec.Command(cmd.Name, cmd.Args...)
 	c.Stdout = out
 	c.Stderr = stderr
 	c.Stdin = in
 	return c.Run()
+}
+
+func (s *Shell) handleBuiltIn(cmd *BuiltInCmd) error {
+	switch cmd.Name {
+	case "cd":
+		return s.cd(cmd.Args...)
+	case "pwd":
+		return s.pwd(cmd.Out)
+	default:
+		return fmt.Errorf("%s: command not found", cmd.Name)
+	}
+}
+
+func (s *Shell) isBuiltIn(name string) bool {
+	_, ok := s.builtIns[name]
+	return ok
 }
